f58a1a68fdf10388ef1bdada6e3ed7e158ab4303
Alex Olieman <alex@olieman.net>
2018-06-23 16:33:09 +0200

    http api

---
+++ same_thing/app.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+from apistar import App, Route
+from apistar.exceptions import NotFound
+
+
+# ensure module is found in path
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+sys.path.insert(0, os.path.abspath(BASE_DIR))
+
+from same_thing.db import get_connection, split_values
+from same_thing.sink import DBP_GLOBAL_MARKER, DBP_GLOBAL_PREFIX
+
+db = get_connection('.', read_only=True)
+
+
+def lookup(uri: str) -> dict:
+    normalized_uri = uri.lstrip(DBP_GLOBAL_PREFIX)
+    if normalized_uri.startswith(DBP_GLOBAL_MARKER):
+        normalized_uri = normalized_uri.encode('utf8')
+    else:
+        normalized_uri = db.get(uri.encode('utf8'))
+        if not normalized_uri:
+            raise NotFound()
+
+    value_bytes = db.get(normalized_uri)
+    if not value_bytes:
+        raise NotFound()
+
+    return {
+        'global': DBP_GLOBAL_PREFIX + normalized_uri.decode('utf8'),
+        'locals': split_values(value_bytes),
+    }
+
+
+routes = [
+    Route('/lookup/', method='GET', handler=lookup),
+]
+
+app = App(routes=routes)
+
+
+if __name__ == '__main__':
+    app.serve('0.0.0.0', 5000, debug=True, use_reloader=True)
---
+++ same_thing/db.py
@@ -0,0 +1,58 @@
+import os
+
+import rocksdb
+from rocksdb import CompressionType, BackupEngine
+from rocksdb.interfaces import AssociativeMergeOperator
+
+DB_ROOT_PATH = '/dbdata'
+SEPARATOR = b'<>'
+
+
+backupper = BackupEngine(DB_ROOT_PATH + '/backups')
+
+
+def get_connection(db_name, db_options=None, read_only=True):
+    if db_options is None:
+        db_options = get_rocksdb_options()
+
+    db_path = os.path.join(DB_ROOT_PATH, db_name)
+    return rocksdb.DB(db_path, db_options, read_only)
+
+
+def split_values(value_bytes):
+    return sorted([
+        val.decode('utf8')
+        for val in value_bytes.split(SEPARATOR)
+    ])
+
+
+class StringAddOperator(AssociativeMergeOperator):
+    def merge(self, key, existing_value, value):
+        new_value = existing_value or value
+        if existing_value and value not in existing_value:
+            new_value = existing_value + SEPARATOR + value
+
+        return True, new_value
+
+    def name(self):
+        return b'StringAddOperator'
+
+
+def get_rocksdb_options():
+    rocks_options = rocksdb.Options()
+    rocks_options.create_if_missing = True
+    rocks_options.merge_operator = StringAddOperator()
+    rocks_options.compression = CompressionType.zstd_compression
+    rocks_options.max_open_files = 300000
+    rocks_options.write_buffer_size = 67 * 1024**2
+    rocks_options.max_write_buffer_number = 3
+    rocks_options.target_file_size_base = 67 * 1024**2
+    rocks_options.max_log_file_size = 4 * 1024**2
+    rocks_options.keep_log_file_num = 100
+
+    rocks_options.table_factory = rocksdb.BlockBasedTableFactory(
+        filter_policy=rocksdb.BloomFilterPolicy(10),
+        block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
+        block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2))
+    )
+    return rocks_options
