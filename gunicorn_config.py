2b6f0fca70a1fe5dcac7aefb89b61dd71e6ad488
Alex Olieman <alex@olieman.net>
2019-03-20 12:44:32 +0100

    reduce number of workers to lower memory requirements

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2
+workers = multiprocessing.cpu_count()
 worker_class = 'uvicorn.workers.UvicornWorker'
 proc_name = 'same-thing'
 log_level = 'warning'
