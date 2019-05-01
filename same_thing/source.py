cceffd26148242904eb041226cef6fac2c11c457
Alex Olieman <alex@olieman.net>
2019-05-01 17:54:13 +0200

    move sparql query to separate file

--- same_thing/source.py
+++ same_thing/source.py
@@ -2,10 +2,13 @@ import asyncio
 import os
 import shutil
 from datetime import datetime, timezone
+from urllib.parse import urlencode, quote_plus
 
 import aiofiles
 import aiohttp
 from tqdm import tqdm
+
+from same_thing.sparql_queries import latest_global_ids
 
 DOWNLOAD_PATH = '/downloads'
 SNAPSHOT_FILENAME = 'global-ids_base58.tsv.bz2'
@@ -18,7 +21,6 @@ async def fetch_latest_snapshot():
     conn = aiohttp.TCPConnector(limit_per_host=10)
     async with aiohttp.ClientSession(connector=conn, raise_for_status=True) as session:
         latest_snapshot, snapshot_url = await find_latest_snapshot(session)
-        assert latest_snapshot, f'No snapshots found. check sparql query to databus or internet connection'
         if latest_snapshot not in existing_downloads:
             # delete older snapshots
             for old_snapshot in existing_downloads:
@@ -34,12 +36,20 @@ async def fetch_latest_snapshot():
 
 
 async def find_latest_snapshot(session):
-    sparql_request = "https://databus.dbpedia.org/repo/sparql?query=PREFIX%20dataid%3A%20%3Chttp%3A%2F%2Fdataid.dbpedia.org%2Fns%2Fcore%23%3E%0APREFIX%20dct%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0APREFIX%20dcat%3A%20%20%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23%3E%0A%0A%23%20Get%20all%20files%0ASELECT%20DISTINCT%20%3Ffile%20%3Flatest%20WHERE%20%7B%0A%09%3Fdataset%20dataid%3Aartifact%20%3Chttps%3A%2F%2Fdatabus.dbpedia.org%2Fdbpedia%2Fid-management%2Fglobal-ids%3E%20.%0A%09%3Fdataset%20dcat%3Adistribution%20%3Fdistribution%20.%0A%09%3Fdistribution%20dataid%3AcontentVariant%20%27base58%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%09%3Fdistribution%20dataid%3AformatExtension%20%27tsv%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%09%3Fdistribution%20dataid%3Acompression%20%27bzip2%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%09%3Fdistribution%20dcat%3AdownloadURL%20%3Ffile%20.%0A%20%20%20%20%7BSELECT%20%3Fdataset%20%3Flatest%20WHERE%20%7B%20%23%20join%20with%20latest%20version%20available%0A%09%09%09%3Fdataset%20dataid%3Aartifact%20%3Chttps%3A%2F%2Fdatabus.dbpedia.org%2Fdbpedia%2Fid-management%2Fglobal-ids%3E%20.%0A%20%20%20%20%20%20%09%09%3Fdataset%20dcat%3Adistribution%20%3Fdistribution%20.%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Fdistribution%20dataid%3AcontentVariant%20%27base58%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Fdistribution%20dataid%3AformatExtension%20%27tsv%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Fdistribution%20dataid%3Acompression%20%27bzip2%27%5E%5E%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23string%3E%20.%0A%09%09%09%3Fdataset%20dct%3AhasVersion%20%3Flatest%20.%0A%09%09%7D%20ORDER%20BY%20DESC(%3Flatest)%20LIMIT%201%20%0A%20%20%7D%0A%7D"
+    payload = {'query': latest_global_ids}
+    parameters = urlencode(payload, quote_via=quote_plus)
+    sparql_request_url = f'https://databus.dbpedia.org/repo/sparql?{parameters}'
     sparql_json_mime = 'application/sparql-results+json'
     headers = {'Accept': sparql_json_mime}
-    async with session.get(sparql_request, headers=headers) as resp:
+    async with session.get(sparql_request_url, headers=headers) as resp:
         assert resp.status == 200, 'Could not GET latest snapshot from Databus'
         global_json = await resp.json(content_type=sparql_json_mime)
+
+    assert (
+        global_json.get('results')
+        and global_json['results'].get('bindings')
+        and len(global_json['results']['bindings'])
+    ), f'No latest snapshot was found on the Databus with query: {latest_global_ids}'
 
     first_binding = global_json['results']['bindings'][0]
     snapshot_name = first_binding['latest']['value']
@@ -60,7 +70,7 @@ async def download_file(session, url, destination_dir=DOWNLOAD_PATH):
     async with session.get(url, timeout=DOWNLOAD_TIMEOUT) as response:
         filename = os.path.basename(url)
         filepath = os.path.join(destination_dir, filename)
-        print_with_timestamp(f'Downloading {filename} to {filepath}')
+        print_with_timestamp(f'Downloading {filename} to {destination_dir}')
 
         if os.path.exists(destination_dir):
             if os.path.exists(filepath):
