2cef1fc0255cddea6900d52599f72c616e5e2e62
Alex Olieman <alex@olieman.net>
2019-05-06 17:14:37 +0200

    save some ram by sharing DB cache (possibly)

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,6 +5,7 @@ from same_thing.db import purge_data_dbs
 bind = "0.0.0.0:8000"
 workers = min(4, multiprocessing.cpu_count())
 worker_class = 'uvicorn.workers.UvicornWorker'
+preload_app = True
 proc_name = 'same-thing'
 loglevel = 'warning'
 
